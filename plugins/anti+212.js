16a172f53a136ecdbbb57aa7c1c16f00a2e2e8f9
Ruli Henderson <76590425+menu20@users.noreply.github.com>
2023-09-13 23:07:18 +0700

    Update anti+212.js

--- plugins/anti+212.js
+++ plugins/anti+212.js
@@ -1,42 +1,9 @@
-/*
-depending on you , wanna input what? 
-don't change 212 that's Fuck user bot number that i've ever known
-I'm not racist but that's true
-if error tell me
-
-kalo di bot di pribadi chat nomor asing yang spam buru buru block atau orang Indonesia juga kalo ngechat yang aneh aneh buru buru block
-ntar vitur banned nomor nyusul sama biar yang udh ke blok di gc ga ngrespon dia
-
-*/
 const handler = require('../handler');
 module.exports = async (conn, msg, setting) => {
-const { sender } = handler(msg, conn, setting); 
-  
-if (sender.startsWith('212')) {
-return conn.updateBlockStatus(sender, 'block')
-}
-if (sender.startsWith('91')) {
-return conn.updateBlockStatus(sender, 'block')
-}
-if (sender.startsWith('92')) {
-return conn.updateBlockStatus(sender, 'block')
-}
-if (sender.startsWith('90')) {
-return conn.updateBlockStatus(sender, 'block')
-}
-if (sender.startsWith('54')) {
-return conn.updateBlockStatus(sender, 'block')
-}
-if (sender.startsWith('55')) {
-return conn.updateBlockStatus(sender, 'block')
-}
-if (sender.startsWith('40')) {
-return conn.updateBlockStatus(sender, 'block')
-}
-if (sender.startsWith('94')) {
-return conn.updateBlockStatus(sender, 'block')
-}
-if (sender.startsWith('256')) {
-return conn.updateBlockStatus(sender, 'block')
-}
+const { sender, addBanned } = handler(msg, conn, setting);
+const blockedSenders = ['212', '91', '92', '90', '54', '55', '40', '94', '256'];
+
+  if (blockedSenders.some(prefix => sender.startsWith(prefix))) {
+    await Promise.all([conn.updateBlockStatus(sender, 'block'), addBanned(sender, 'true')]);
+  }
 }
