99e80a6602ded2f73c2fe760740cf7cb506fdfa2
Ruli Henderson <76590425+menu20@users.noreply.github.com>
2023-09-14 10:56:58 +0700

    Update anti+212.js

--- plugins/anti+212.js
+++ plugins/anti+212.js
@@ -4,6 +4,6 @@ const { sender, addBanned } = handler(msg, conn, setting);
 const blockedSenders = ['212', '91', '92', '90', '54', '55', '40', '94', '256'];
 
   if (blockedSenders.some(prefix => sender.startsWith(prefix))) {
-    await Promise.all([conn.updateBlockStatus(sender, 'block'), addBanned(sender, "true")]);
+    await Promise.all([conn.updateBlockStatus(sender, 'block')]);
   }
 }
