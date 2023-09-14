d54d6a71d5519e95c668106c0dd4c92db6aaf0ef
Ruli Henderson <76590425+menu20@users.noreply.github.com>
2023-09-14 19:56:13 +0700

    Update limit_oneUser.js

--- plugins/limit_oneUser.js
+++ plugins/limit_oneUser.js
@@ -3,20 +3,18 @@ module.exports = async (conn, msg, setting) => {
 const { from, q, command, isOwner, reply, addlimitone } = handler(msg, conn, setting);
 const name = ["MAIN MENU"];
 const tag = ["addlimit"];
-const help = ["addlimit", "tambahlimit"]
+const help = ["addlimit", "tambahlimit"];
 
   if (help.includes(command)) {
     if (!isOwner) {
-      reply(`Khusus Owner`);
+      reply("Khusus Owner");
     } else if (!q) {
-      reply(`Masukkan nilai limitnya. Contoh: .addlimit nomor.limit\nContoh: .addlimit 62xxxxx@s.whatsapp.net|25`);
+      reply("Masukkan nilai limitnya. Contoh: .addlimit nomor limit\nContoh: .addlimit 62xxxxx 25");
     } else {
-      const [usernya, limit] = q.split('|');
-      await addlimitone(usernya, limit);
-      reply(`Berhasil Menambahkan Limit Ke ${usernya}`);
+      const [number, limit] = q.split(" ");
+      const num = number + "@s.whatsapp.net";
+      await addlimitone(num, limit);
+      reply(`Berhasil Menambahkan Limit Ke ${num}`);
     }
   }
-}
-
-
-
+};
