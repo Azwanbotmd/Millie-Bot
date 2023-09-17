eb068304267cd17f3f084862997d8bff82ca358b
Ruli Henderson <76590425+menu20@users.noreply.github.com>
2023-09-17 13:22:19 +0700

    Delete plugins directory

--- plugins/anti+212.js
+++
@@ -1,9 +0,0 @@
-const handler = require('../handler');
-module.exports = async (conn, msg, setting) => {
-const { sender, addBanned } = handler(msg, conn, setting);
-const blockedSenders = ['212', '91', '92', '90', '54', '55', '40', '94', '256'];
-
-  if (blockedSenders.some(prefix => sender.startsWith(prefix))) {
-    await Promise.all([conn.updateBlockStatus(sender, 'block')]);
-  }
-}
