fbb30afd381ce1fb5bce206e155b917a9dfed186
Ruli Henderson <76590425+menu20@users.noreply.github.com>
2023-09-20 13:12:16 +0700

    Update user_profile.js

--- plugins/user_profile.js
+++ plugins/user_profile.js
@@ -16,12 +16,10 @@ const help = ["profile", "my"];
      const prem = isPremium ? 'Aktif' : 'Tidak';     
      const isRegister = checkRegisteredUser(msg.sender);
      const reg = isRegister ? 'Sudah Daftar' : 'Belum Daftar';     
-     const limitUser = checkLimitUser(msg.sender);
-     const remainingHour = await Hour(msg.sender); 
-     const userData = await getProfileData(msg.sender);
-     const remainingClaim = await claim(msg.sender);
+     const limitUser = checkLimitUser(msg.sender);     
+     const userData = await getProfileData(msg.sender);     
      if (userData) {
-       msg.reply(`Profile Kamu\n\nTerdaftar: ${reg}\nPremium: ${prem}\nNama: ${userData.nama}\nUmur: ${userData.umur}\nSeri: ${userData.seri}\nLimit: ${limitUser}\nUang: ${userData.uang}\nKupon: ${userData.kupon}\nLevel: ${userData.level}\nLast Claim Hour: ${remainingHour} Menit Lagi\nLast Claim Daily: ${remainingClaim} Jam Lagi`);
+       msg.reply(`Profile Kamu\n\nTerdaftar: ${reg}\nPremium: ${prem}\nNama: ${userData.nama}\nUmur: ${userData.umur}\nSeri: ${userData.seri}\nLimit: ${limitUser}\nUang: ${userData.uang}\nKupon: ${userData.kupon}\nLevel: ${userData.level}\n`);
      } else {
        msg.reply('Profil tidak ditemukan');
      }
