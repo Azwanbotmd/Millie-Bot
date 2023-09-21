0ac29c1b47bc41a84ebeac277d5227211ea676fc
Ruli Henderson <76590425+menu20@users.noreply.github.com>
2023-09-22 00:32:55 +0700

    Update user_claimhour.js

--- plugins/user_claimhour.js
+++ plugins/user_claimhour.js
@@ -8,19 +8,19 @@ const name = ["USER MENU"];
 const tag = ["hour"];
 const help = ["hour"];
 
+try {
   if (help.includes(command)) {
     if (!isRegister) return msg.reply(mess.daftar);
-
-    var hourly = 5;
-
+    var hourly = 5
     const remainingTime = await Hour(msg.sender, hourly);
-
-    if (typeof remainingTime === "number") {
-      
+    if (typeof remainingTime === "number") {      
       return msg.reply(`Kamu sudah melakukan claim dalam 1 jam terakhir. Tunggu ${remainingTime} menit lagi sebelum dapat melakukan claim kembali.`);
     } else {
-      
+      msg.reply(`Claim Perjam berhasil. Kamu mendapatkan ${hourly} Limit. Kamu bisa melakukan claim lagi dalam 1 jam mendatang.`);
      // return msg.reply(`Silahkan Claim Perjam .hour`);
-    }
+       }
+     }
+   } catch (error) {
+   msg.reply("Data pengguna tidak ditemukan.");
   }
 };
