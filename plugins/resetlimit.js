9ea8e7d1f534fc987d7414430121b04cf368cbd2
Ruli Henderson <76590425+menu20@users.noreply.github.com>
2023-09-22 00:34:28 +0700

    Update resetlimit.js

--- plugins/resetlimit.js
+++ plugins/resetlimit.js
@@ -1,20 +1,23 @@
-exports.on = async ( msg, { conn, command, text, isOwner, resetLimits }) => {
+exports.on = async (msg, { conn, command, text, isOwner, resetLimits }) => {
 const name = ["OWNER"];
 const tag = ["resetlimit"];
 const help = ["resetlimit"];
-
-  if (help.includes(command)) {
-    if (!isOwner) {
-      msg.reply(`Hanya Dapat Di Gunakan Oleh Owner`);
-      return;
+  
+  try {   
+    if (help.includes(command)) {
+      if (!isOwner) {
+        msg.reply(`Hanya Dapat Di Gunakan Oleh Owner`);
+        return;
+      }
+      if (!text) {
+        msg.reply(`Masukan Nilai Limit Yang Ingin Di Reset Ke Semua Pengguna\ncontoh .resetlimit 25`);
+        return;
+      }
+      await resetLimits(text);
+      const a = `Berhasil Mereset Limit\n${text} Per User`;
+      conn.sendMessage(msg.chat, { text: a }, { quoted: msg });
     }
-    if (!text) {
-      msg.reply(`Masukan Nilai Limit Yang Ingin Di Reset Ke Semua Pengguna\n contoh .resetlimit 25`);
-      return;
-    }
-    await resetLimits(text);
-    const a = `Berhasil Mereset Limit\n${text} Per User`;
-    conn.sendMessage(msg.chat, { text: a }, { quoted: msg });
+  } catch (error) {
+    msg.reply(`Gagal Melakukan Reset Limit`);
   }
-
 };
