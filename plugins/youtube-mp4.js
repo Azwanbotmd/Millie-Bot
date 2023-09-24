b96646a4fb8ba4fb0dc93600f8407497371111da
Ruli Henderson <76590425+menu20@users.noreply.github.com>
2023-09-24 18:28:21 +0700

    Update youtube-mp4.js

--- plugins/youtube-mp4.js
+++ plugins/youtube-mp4.js
@@ -1,16 +1,16 @@
-const ytmp4 = require ('@ruhend/ytmp4');
+const { ytmp4 } = require ('@ruhend/scraper');
 exports.on = async ( msg, { conn, command, text, mess, ceklimit, limitnya }) => {
 const name = ["DOWNLOADER"];
 const tag = ["ytmp4"];
 const help = ["ytmp4", "ytv"]
 const limit = 4
-
-   if (help.includes(command)) {      
+  
+  if (help.includes(command)) {      
       if (ceklimit) return msg.reply(mess.limit)
-      if (!text) return msg.reply('*Masukan Link Youtubenya*');
-      msg.reply(mess.wait)
-      let res = await ytmp4(text);      
-      conn.sendFile(msg.chat, res.download, { caption : `🎗 *YOUTUBE VIDEO*\n*Judul :* ${res.title}`, quoted : msg } )
-      limitnya(msg.sender, limit)       
+      if (!text) return msg.reply('*Masukan Link Youtubenya*');      
+      msg.reply(`Tunggu...`)
+      let { title, video } = await ytmp4(text);      
+      conn.sendFile(msg.chat, video, { caption : `🎗Youtube Video \nJudul : ${title}` , quoted : msg })
+      limitnya(msg.sender, limit)      
    }
 }
