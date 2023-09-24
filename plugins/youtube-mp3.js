d33e2eb29177c938715f9578bcb9de84291f7e8e
Ruli Henderson <76590425+menu20@users.noreply.github.com>
2023-09-24 18:29:01 +0700

    Update youtube-mp3.js

--- plugins/youtube-mp3.js
+++ plugins/youtube-mp3.js
@@ -1,18 +1,18 @@
-const ytmp3 = require ('@ruhend/ytmp3');
+const { ytmp3 } = require ('@ruhend/scraper');
 exports.on = async ( msg, { conn, command, text, mess, ceklimit, limitnya }) => {
 const name = ["DOWNLOADER"];
 const tag = ["ytmp3"];
 const help = ["ytmp3", "yta"]
 const limit = 3
-  
+    
   if (help.includes(command)) {      
       if (ceklimit) return msg.reply(mess.limit)
       if (!text) return msg.reply('*Masukan Link Youtubenya*');      
       msg.reply(`Tunggu...`)
-      let audio = await ytmp3(text);      
+      let { title, audio } = await ytmp3(text);      
       conn.sendMessage(msg.chat, {
-        document: { url: audio.download },
-        fileName: `${audio.title}~RuhendMD.mp3`,
+        document: { url: audio },
+        fileName: `${title}~RuhendMD.mp3`,
         mimetype: 'audio/mpeg'
       }, { quoted: msg })
       limitnya(msg.sender, limit)      
