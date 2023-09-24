dc164e41a3b20fa88af4d028f26be9ec4af067e8
Ruli Henderson <76590425+menu20@users.noreply.github.com>
2023-09-24 18:30:09 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,4 +1,4 @@
-const ttdl = require ('@ruhend/tiktok');
+const { ttdl } = require ('@ruhend/scraper');
 exports.on = async ( msg, { conn, command, mess, text, limitnya, ceklimit }) => { try {
 const name = ["DOWNLOADER"];
 const tag = ["tiktok", "titit"];  
@@ -8,9 +8,18 @@ const limit = 2
  if (help.includes(command)) {   
     if (ceklimit) return msg.reply(mess.limit)
     if (!text) return msg.reply('Masukan tiktok contoh .tiktok https://vt.tiktok.com/ZSLgavgpQ/');
-    msg.reply(mess.wait)
-    let res = await ttdl(text)    
-    conn.sendFile(msg.chat, res.video, { caption : `🎗 *TIKTOK*\nUsername: ${res.author}\nJudul : ${res.title}`, quoted : msg } )
+    msg.reply(mess.wait)    
+    let { video , title , nickname , username , duration , comment, playcount , share } = await ttdl(text)
+    conn.sendFile(msg.chat, video, { caption : `🎗 *TIKTOK*
+Username: ${nickname}
+Judul : ${title}
+username : ${username}
+duration : ${duration}
+comment : ${comment}
+playcount : ${playcount}
+share : ${share}`
+,quoted : msg } )
+    
     limitnya(msg.sender, limit)    
      }
    } catch (error) {
