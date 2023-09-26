cdcf90aea939537217924aae55970a5766c6818f
Ruli Henderson <76590425+menu20@users.noreply.github.com>
2023-09-26 14:21:02 +0700

    Update user_claimhour.js

--- plugins/user_claimhour.js
+++ plugins/user_claimhour.js
@@ -14,7 +14,7 @@ try {
     var hourly = 5
     const remainingTime = await Hour(msg.sender, hourly);
     if (typeof remainingTime === "number") {      
-      return msg.reply(`Kamu sudah melakukan claim dalam 1 jam terakhir. Tunggu ${remainingTime} menit lagi sebelum dapat melakukan claim kembali.`);
+      return msg.reply(`Kamu sudah melakukan claim dalam 1 jam terakhir. Tunggu ${remainingTime} menit lagi sebelum dapat melakukan claim kembali.\nkamu juga bisa claim uang  ketik .claimuang`);
     } else {
       msg.reply(`Claim Perjam berhasil. Kamu mendapatkan ${hourly} Limit. Kamu bisa melakukan claim lagi dalam 1 jam mendatang.`);
      // return msg.reply(`Silahkan Claim Perjam .hour`);
