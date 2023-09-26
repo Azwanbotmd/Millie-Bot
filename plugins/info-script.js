aea9f32a14e939ece479bfe72ec61e39bba32b6e
Ruli Henderson <76590425+ruhend2001@users.noreply.github.com>
2023-09-26 21:05:58 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -4,7 +4,7 @@ const tag = ["script"];
 const help = ["script", "sc", "repo", "repositori"];
 
   if (help.includes(command)) {
-   const script = 'Im Currently Using This\n\nhttps://github.com/menu20/Millie-Bot'
+   const script = 'Im Currently Using This\n\https://github.com/ruhend2001/Millie-Bot'
     conn.sendMessage(msg.chat, { text: script }, { quoted: msg });      
    }
 };
