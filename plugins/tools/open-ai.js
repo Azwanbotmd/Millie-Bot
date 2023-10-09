e008e4df9b43edc80165c683dabf0d5b4639847c
Ruli Henderson <76590425+ruhend2001@users.noreply.github.com>
2023-10-09 23:38:48 +0700

    Add files via upload

---
+++ plugins/tools/open-ai.js
@@ -0,0 +1,17 @@
+import fetch from 'node-fetch'
+export let on = async (m, {
+    text
+}) => {
+    if (!text) return m.reply('contoh .ai apa kabar?')
+    let res = await fetch(`https://vihangayt.me/tools/chatgpt?q=${text}`)
+    m.reply('Menunggu Respon...')
+    let open = await res.json()
+    let ai = await open.data
+    m.reply(`${ai}`)
+};
+
+on.names = ['Tools'];
+on.tags = ['ai', 'chatgpt'];
+on.command = ['ai', 'chatgpt'];
+on.limit = 5
+on.register = true
