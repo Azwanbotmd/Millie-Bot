5e1f3c10daaa7e3d5b85170fd768e11efbf04f54
Ruli Henderson <76590425+ruhend2001@users.noreply.github.com>
2023-10-09 09:59:50 +0700

    Delete function directory

--- function/Data_Location.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-JSON_DATA = {
-setting: JSON.parse(fs.readFileSync('./config.json')),
-mess: JSON.parse(fs.readFileSync('./function/message.json')),
-}
-
-exports.setting_JSON = JSON_DATA.setting;
-exports.mess_JSON = JSON_DATA.mess;
