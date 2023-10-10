7c8d7536a0621be3ec349a6c9f4ae39a93903c66
Ruli Henderson <76590425+ruhend2001@users.noreply.github.com>
2023-10-10 07:55:24 +0700

    Add files via upload

---
+++ plugins/maker/sticker.js
@@ -0,0 +1,28 @@
+export let on = async (m, {
+    conn,
+    prefix,
+    command,
+    mime,
+    mess,
+    quoted
+}) => {
+    let pack = `Millie-Bot`;
+    let own = `Ruhend`;
+
+    if (/image/.test(mime) || m.mtype === 'imageMessage') {
+        if (!quoted) return
+        let buffer = await quoted.download()
+        m.reply(mess.wait);
+        conn.sendImageAsSticker(m.chat, buffer, m, {
+            packname: pack,
+            author: own
+        });
+    } else {
+        m.reply(`Kirim gambar atau video dengan caption ${prefix + command} atau balas gambar yang sudah dikirim`);
+    }
+};
+
+on.names = ['Maker'];
+on.tags = ['sticker'];
+on.command = ['sticker', 's', 'stiker']
+on.limit = 2
