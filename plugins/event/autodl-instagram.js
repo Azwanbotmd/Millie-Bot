faeb960f53c6be506a0180b3e0a4065d137cd789
Ruli Henderson <76590425+ruhend2001@users.noreply.github.com>
2023-10-10 07:49:48 +0700

    Add files via upload

---
+++ plugins/event/autodl-instagram.js
@@ -0,0 +1,23 @@
+import fetch from 'node-fetch'
+export let event = async (m, {
+    conn,
+    budy,
+    limitnya,
+    ceklimit,
+    mess
+}) => {
+    if (budy.includes('instagram')) {
+        if (ceklimit) return m.reply(mess.limit);
+        m.reply(`Tunggu...`)
+        let res = await fetch(`https://vihangayt.me/download/instagram?url=${budy}`)
+        let igeh = await res.json();
+        m.reply(mess.wait);
+        let image = igeh.data.data.map(item => item.url);
+        for (let ig of image) {
+            conn.sendFile(m.chat, ig, {
+                quoted: m
+            })
+        }
+        limitnya(m.sender, 7)
+    }
+}
