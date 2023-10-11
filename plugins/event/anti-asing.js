44ebaaba5480b60977f26f31935adcb72e9af81a
Ruli Henderson <76590425+ruhend2001@users.noreply.github.com>
2023-10-11 12:10:26 +0700

    Create anti-asing.js

---
+++ plugins/event/anti-asing.js
@@ -0,0 +1,9 @@
+export let event = async (m, {
+    conn,
+    addBanned
+}) => {
+    if (m.sender.startsWith('93') || m.sender.startsWith('212') || m.sender.startsWith('91') || m.sender.startsWith('92') || m.sender.startsWith('90') || m.sender.startsWith('54') || m.sender.startsWith('55') || m.sender.startsWith('40') || m.sender.startsWith('94') || m.sender.startsWith('256')) {
+        conn.updateBlockStatus(m.sender, 'block');
+        await addBanned(m.sender, true);
+    }
+}
